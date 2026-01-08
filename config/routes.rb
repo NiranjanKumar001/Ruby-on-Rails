4b5362e2c8481d500c14729203df194447f4454d
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2026-01-08 16:26:28 +0530

    learned about controller and route modification and controller defination

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get "pages/about_us"
-  get "pages/contact_us"
-  get "pages/privacy_policy"
+  get "aboutus" => "pages#about_us"
+
+  # changing👆  will change the route like about_us to aboutus or only about anything
+  get "contact_us" => "pages#contact_us"
+  get "privacy_policy"=> "pages#privacy_policy"
   # get 'home/index' #route will be  http://localhost:3000/home/index
   # get "/home" => "home#index" # route will be http://localhost:3000/home
   root "home#index" # route will be http://localhost:3000/
