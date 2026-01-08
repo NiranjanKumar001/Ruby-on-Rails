e5412d9fb1c24fb5461e5ac7fb7aaf7f64a8c129
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2026-01-08 16:17:30 +0530

    generating the controller view and routes using generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "pages/about_us"
+  get "pages/contact_us"
+  get "pages/privacy_policy"
   # get 'home/index' #route will be  http://localhost:3000/home/index
   # get "/home" => "home#index" # route will be http://localhost:3000/home
   root "home#index" # route will be http://localhost:3000/
