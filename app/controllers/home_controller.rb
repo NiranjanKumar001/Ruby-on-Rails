44dc09da9c2bc728d995c024664486f10cc89420
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2026-01-08 16:08:44 +0530

    learning routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
     def index
+      @title = "Welcome to My First Rails App"
+      @subtitle = "Building with Rails and Tailwind CSS"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index' #route will be  http://localhost:3000/home/index
+  # get "/home" => "home#index" # route will be http://localhost:3000/home
+  root "home#index" # route will be http://localhost:3000/
 end
